7d20b1ee6afb00ddcc304386b4ce50d0263439da
Arjan de Ruijter <rra@mboutrecht.nl>
2015-09-30 11:27:16 +0200

    form

--- update_personalia.php
+++ update_personalia.php
@@ -9,5 +9,32 @@
 	
 	include("db_connect.php");
 	
+	$query = "SELECT * FROM `personalia` WHERE `id` = '".$_GET['id']."' ";
 	
+	$result = mysqli_query($conn, $query);
+	
+	$record = mysqli_fetch_assoc($result);
+	
+	var_dump($record);
 ?>
+
+<form>
+	<table>
+		<tr>
+			<td>voornaam</td>
+			<td>
+				<input type='text' 
+					   name='voornaam'
+					   value='<?php echo $record['voornaam']; ?>'>			
+			</td>
+		</tr>
+		<tr>
+			<td>tussenvoegsel</td>
+			<td>
+				<input type='text' 
+					   name='tussenvoegsel'
+					   value='<?php echo $record['tussenvoegsel']; ?>'>			
+			</td>
+		</tr>
+	</table>
+</form>
